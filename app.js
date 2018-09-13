61c65b90e2668358a6cf18e580dc105ed3f806d0
Naz Islam <naz@nazislam.com>
2018-09-13 18:54:01 -0400

    Added package.json

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function(req, res) {
+  res.send('Hello world!');
+})
+
+app.listen(port, function() {
+  console.log('server is running on port ' + port);
+})
