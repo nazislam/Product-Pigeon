5ee4e640078a651eec0dc718414d1c83deb337d0
Naz Islam <naz@nazislam.com>
2018-09-20 19:33:56 -0400

    Added routing functionality using Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { RegistrationComponent } from './registration/registration.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'registration', component: RegistrationComponent}
+      {path: 'signIn', component: SignInComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
